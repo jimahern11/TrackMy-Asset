edc2007ec08f29f75002df53339c1417c52447f9
jimahern11 <jimahern11@gmail.com>
2020-05-05 21:11:14 +0100

    testing post api

--- trips_app/views.py
+++ trips_app/views.py
@@ -77,8 +77,8 @@ def map(request):
 
     print('what is here',type(payload.decode()))
     print('what is the length of payload',len(payload))
-    response = requests.request("GET", params=querystring)
-    json_string = json.dumps(response)
+    #response = requests.request("GET", params=querystring)
+    #json_string = json.dumps(response)
 
     context = {
         'latitude' : 'request.latitude'
